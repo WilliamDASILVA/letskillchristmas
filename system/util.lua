619cdc9c436ea4e8323eff7d1d81421f409c03cf
William DA SILVA <william.da.silva@outlook.com>
2014-12-29 03:13:09 +0100

    Adding the function findrotation

--- system/util.lua
+++ system/util.lua
@@ -51,3 +51,18 @@ function isRectangleInRectangle(x, y, w, h, a, b, c, d)
 	end
 	return did;
 end
+
+--[[
+			[function] findRotation()
+	
+			* Find the rotation between 2 points *
+	
+			Return: 
+]]
+function findRotation(x1,y1,x2,y2)
+ 
+  local t = -math.deg(math.atan2(x2-x1,y2-y1))
+  if t < 0 then t = t + 360 end;
+  return t;
+ 
+end
